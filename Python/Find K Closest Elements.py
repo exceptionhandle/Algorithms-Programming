d4fb239b712e0a1dd5ab7cb66b695f90aab48b35
Keshav Sharma <keshav7890@gmail.com>
2024-03-14 23:08:40 -0700

    Create  Find K Closest Elements.py

---
+++ Python/Find K Closest Elements.py
@@ -0,0 +1,44 @@
+Given a sorted integer array arr, two integers k and x, return the k closest integers to x in the array. The result should also be sorted in ascending order.
+
+An integer a is closer to x than an integer b if:
+
+|a - x| < |b - x|, or
+|a - x| == |b - x| and a < b
+ 
+
+Example 1:
+
+Input: arr = [1,2,3,4,5], k = 4, x = 3
+Output: [1,2,3,4]
+Example 2:
+
+Input: arr = [1,2,3,4,5], k = 4, x = -1
+Output: [1,2,3,4]
+ 
+
+Constraints:
+
+1 <= k <= arr.length
+1 <= arr.length <= 104
+arr is sorted in ascending order.
+-104 <= arr[i], x <= 104
+
+class Solution:
+    def findClosestElements(self, arr: List[int], k: int, x: int) -> List[int]:
+        if len(arr) == k:
+            return arr
+        
+        left = bisect_left(arr, x) - 1
+        right = left + 1
+
+        while right - left - 1 < k:
+            if left == -1:
+                right += 1
+                continue
+
+            if right == len(arr) or abs(arr[left] - x) <= abs(arr[right] - x):
+                left -= 1
+            else:
+                right += 1
+
+        return arr[left + 1: right]
